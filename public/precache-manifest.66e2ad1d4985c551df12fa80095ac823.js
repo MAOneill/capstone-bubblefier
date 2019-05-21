ca9edac233a1a9648f42f30592722192ffdecf89
Margaret O'Neill <margaret@oneillfish.com>
2019-05-21 14:47:58 -0400

    added frontend build to we could get access to sessions userObjecty

---
+++ public/precache-manifest.66e2ad1d4985c551df12fa80095ac823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c751a46fb4b55e95362611a9df3a4f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5b32caa030d754d5790",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1e593b28a1f15f0a74e8",
+    "url": "/static/js/2.84e8a9a0.chunk.js"
+  },
+  {
+    "revision": "f5b32caa030d754d5790",
+    "url": "/static/js/main.d2ab9355.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ public/precache-manifest.6e5c729057d185a44249b6564ac4401d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0becdb27906055b0af52449ae47f48df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92005491c0b3bba8b704",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1e593b28a1f15f0a74e8",
+    "url": "/static/js/2.84e8a9a0.chunk.js"
+  },
+  {
+    "revision": "92005491c0b3bba8b704",
+    "url": "/static/js/main.02297e2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
