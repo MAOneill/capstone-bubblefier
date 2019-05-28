54c722a235e32d5d8e743b9012b2a16954b69242
Margaret O'Neill <margaret@oneillfish.com>
2019-05-28 16:27:58 -0400

    adding build to production branch

---
+++ public/precache-manifest.666fec7af64e3edc87b83ff16066a97a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d9142cb803ea7a49600c6bbd699ca5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26ddbca28f5ea5615d20",
+    "url": "/static/css/main.262219af.chunk.css"
+  },
+  {
+    "revision": "b53197cd477023d57d4e",
+    "url": "/static/js/2.22542ea2.chunk.js"
+  },
+  {
+    "revision": "26ddbca28f5ea5615d20",
+    "url": "/static/js/main.0463cf0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
