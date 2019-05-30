a8bd7e25c5fc3aa542bdac7c9f018d56b09664b8
Margaret O'Neill <margaret@oneillfish.com>
2019-05-30 12:47:54 -0400

    new build files

---
+++ public/precache-manifest.6a785a9c8e8ee7961efa51b63731ceea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6fc430ba1fafd25733038ec6145142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4aa29be16c1a5ca1041",
+    "url": "/static/css/main.60cf2eb9.chunk.css"
+  },
+  {
+    "revision": "80269bc601de958f3abd",
+    "url": "/static/js/2.9f47db94.chunk.js"
+  },
+  {
+    "revision": "b4aa29be16c1a5ca1041",
+    "url": "/static/js/main.4d7f23e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
