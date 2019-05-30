c8bbf99a37da875eb9aec14f52116f88dd89dabf
Margaret O'Neill <margaret@oneillfish.com>
2019-05-30 13:06:58 -0400

    yet another prod build

---
+++ public/precache-manifest.da0904c4ccc5228084041828d42f3f48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2c7211466dbd8ff3142ab07a8ad0b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c9e68aaf39a4bd737a",
+    "url": "/static/css/main.60cf2eb9.chunk.css"
+  },
+  {
+    "revision": "80269bc601de958f3abd",
+    "url": "/static/js/2.9f47db94.chunk.js"
+  },
+  {
+    "revision": "30c9e68aaf39a4bd737a",
+    "url": "/static/js/main.f339df0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a785a9c8e8ee7961efa51b63731ceea.js"
+  "/precache-manifest.da0904c4ccc5228084041828d42f3f48.js"
 );
 
 self.addEventListener('message', (event) => {
