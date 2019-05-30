e00c820bb78c0ffefd3095289234a98e63569170
Margaret O'Neill <margaret@oneillfish.com>
2019-05-30 12:46:53 -0400

    deleting static old build files

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b0bb35cdecd5bae7714d4d81103e38c.js"
+  "/precache-manifest.6a785a9c8e8ee7961efa51b63731ceea.js"
 );
 
 self.addEventListener('message', (event) => {
